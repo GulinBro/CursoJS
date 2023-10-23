b2e0cd2962f32cf1ce6f37341020ee18f497899d
Gustavo Brendon <guhgomesyt@gmail.com>
2023-10-23 10:16:36 -0300

    Separando Script do HTML, para arquivos JavaScript

---
+++ Aula10/ex006.js
@@ -0,0 +1,18 @@
+var a = document.getElementById('area')
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
+function clicar(){
+    a.innerText = 'Clicou!'
+    a.style.background = 'red'
+}
+
+function entrar(){
+    a.innerText = 'Entrou!'
+    a.style.background = 'blue'
+}
+function sair(){
+    a.innerText = 'Saiu!'
+    a.style.background = 'rgb(15, 121, 15)'
+}
